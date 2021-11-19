e21d8ffaf09b66ef9000312c6b606428577c868b
asurashen <39765452+asurashen@users.noreply.github.com>
2021-11-19 14:54:40 -0500

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# Based mostly on the Switchboard recipe. The training database is TED-LIUM,
+# it consists of TED talks with cleaned automatic transcripts:
+#
+# https://lium.univ-lemans.fr/ted-lium3/
+# http://www.openslr.org/resources (Mirror).
+#
+# The data is distributed under 'Creative Commons BY-NC-ND 3.0' license,
+# which allow free non-commercial use, while only a citation is required.
+#
+# Copyright  2014  Nickolay V. Shmyrev
+#            2014  Brno University of Technology (Author: Karel Vesely)
+#            2016  Vincent Nguyen
+#            2016  Johns Hopkins University (Author: Daniel Povey)
+#            2018  François Hernandez
+#
+# Apache 2.0
+#
+
+. ./cmd.sh
+. ./path.sh
+
+
+set -e -o pipefail -u
+
+nj=35
+decode_nj=38   # note: should not be >38 which is the number of speakers in the dev set
+               # after applying --seconds-per-spk-max 180.  We decode with 4 threads, so
+               # this will be too many jobs if you're using run.pl.
+stage=0
+train_rnnlm=false
+train_lm=false
+
+. utils/parse_options.sh # accept options
+
+# Data preparation
+if [ $stage -le 0 ]; then
+  local/download_data.sh
+fi
+
+if [ $stage -le 1 ]; then
+  local/prepare_data.sh
+  # Split speakers up into 3-minute chunks.  This doesn't hurt adaptation, and
+  # lets us use more jobs for decoding etc.
+  # [we chose 3 minutes because that gives us 38 speakers for the dev data, which is
+  #  more than our normal 30 jobs.]
+  for dset in dev test train; do
+    utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 data/${dset}.orig data/${dset}
+  done
+fi
+
+
+if [ $stage -le 2 ]; then
+  local/prepare_dict.sh
+fi
+
+if [ $stage -le 3 ]; then
+  utils/prepare_lang.sh data/local/dict_nosp \
+    "<unk>" data/local/lang_nosp data/lang_nosp
+fi
+
+
+echo "$0: success."
+exit 0
